a6f151536bbf04b03c2866a5f2b22a7fd39695e8
Altair <dauletuly.altair.09.10.02@gmail.com>
2022-10-07 17:10:52 +0600

    Start app

---
+++ src/main/java/com/hospital/isd3project/Isd3ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.isd3project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Isd3ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Isd3ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hospital/isd3project/Isd3ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.isd3project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Isd3ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
